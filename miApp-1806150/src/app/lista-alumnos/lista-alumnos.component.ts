2760592e54f06c1a05dfad9950d12a95be09c03d
Diego121221 <69687715+Alexuno-ai@users.noreply.github.com>
2023-09-26 19:26:05 -0600

    practica7
    Lista alumnos

--- miApp-1806150/src/app/lista-alumnos/lista-alumnos.component.ts
+++ miApp-1806150/src/app/lista-alumnos/lista-alumnos.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-lista-alumnos',
   templateUrl: './lista-alumnos.component.html',
-  styleUrls: ['./lista-alumnos.component.css']
+  styleUrls: ['./lista-alumnos.component.scss']
 })
 export class ListaAlumnosComponent {
-  alumnos: any[] = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
+  alumnos: any = ["Carlos", "Ximena", "Alberto", "Eduardo", "Baraba", "Georgina", "Kevin"];
 }
 
