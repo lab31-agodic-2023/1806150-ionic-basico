bb6170bec815f4dc8bbf29f7e808ce9d0f1678b1
Alexuno-Ai <inazuma11once@gmail.com>
2023-10-18 17:25:18 -0600

    actualizacion practica 9

--- miApp-1806150/src/app/productos/productos.component.ts
+++ miApp-1806150/src/app/productos/productos.component.ts
@@ -12,7 +12,7 @@ export class ProductosComponent  implements OnInit {
   constructor(private consulta: ConsultaService) { }
 
   ngOnInit() {
-    this.obtenerProductos()
+    this.obtenerProductos();
  }
     obtenerProductos(): void{
       this.consulta.getProductos().subscribe((resp: Object) => {
